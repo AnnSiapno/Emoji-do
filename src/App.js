40abc1c307d3b0eaff388c35b7b2239bd995597b
Ann Siapno <ann.n.siapno@gmail.com>
2017-06-13 14:23:34 +1000

    Enable changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem'
 
 class App extends Component {
   state = {
     items: [
       {
         completed: false,
-        description: "Buy food"
+        description: 'Buy food'
       },
       {
         completed: true,
-        description: "Invest in bitcoin"
+        description: 'Invest in bitcoin'
       }
     ]
   }
+
+  // 1. User interaction
+  // 2. Browser event
+  // 3. Action handler
+  // 4. Changes the state
+  // 5. Rerenders our app
+  // 6. Users see the updated user interface
+
+  handleToggleCompleteItem = (index) => {
+    this.setState((prevState) => {
+      const newItems = prevState.items
+      // Update the correct item in the array
+      newItems[index].completed = !newItems[index].completed
+
+      return {
+        // Hey update this.state.items to what’s in the items variable here
+        items: newItems
+      }
+    })
+  }
+
+  handleChangeItemDescription = (index, newDescription) => {
+    this.setState((prevState) => {
+      const newItems = prevState.items
+      // Update the correct item in the array
+      newItems[index].description = newDescription
+
+      return {
+        // Hey update this.state.items to what’s in the items variable here
+        items: newItems
+      }
+    })
+  }
+
   render() {
     const items = this.state.items
     return (
       <main>
-        <h1>EmojiDo</h1>
+        <h1>Emojido</h1>
         {
-          items.map((item) => {
+          items.map((item, itemIndex) => {
             const completed = item.completed
             const description = item.description
             return (
-              <div>
-              <input type='checkbox' checked={ completed }/>
-                { description }
-              </div>
+              <TodoItem
+                completed={ completed }
+                description={ description }
+                onToggleComplete={
+                  (event) => {
+                    this.handleToggleCompleteItem(itemIndex)
+                  }
+                }
+                onChangeDescription={
+                  (event) => {
+                    const htmlInput = event.target
+                    const newDescription = htmlInput.value
+                    this.handleChangeItemDescription(itemIndex, newDescription)
+                  }
+                }
+              />
             )
           })
         }
